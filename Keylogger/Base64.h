ab2a5180094062dc68dfdc8218a6121ac9ad6a12
htmercury <kawong2020@u.northwestern.edu>
2017-12-08 21:29:41 -0600

    Finished writing the bas64_encoding to encode the given string using base64

--- Keylogger/Base64.h
+++ Keylogger/Base64.h
@@ -30,6 +30,35 @@ namespace Base64
 		s.insert(7, "A");
 		return s;
 	}
+
+	const std::string &BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+
+	std::string base64_encode(const std::string &s)
+	{
+		std::string ret;
+		int val = 0; // index we use map to table
+		int bits = -6; // used to represent number of bits in a subgroup (of six)
+		const unsigned int b63 = 0x3F; // decimal value is 63
+
+		for (const auto &c : s) // auto when type is unknown, c is assign every character from s string
+		{
+			val = (val << 8) + c; // left binary shift by 8, same as val * 2^8 + c
+			bits += 8; // add 8 to numbers of bits when extracting info (octets)
+			while (bits >= 0)
+			{
+				ret.push_back(BASE64_CODES[(val >> bits) & b63]); // does binary right shift + binary AND comparison
+				bits -= 6;
+			}
+		}
+
+		if (bits > -6) // means at least one character has been insert
+			ret.push_back(BASE64_CODES[((val << 8) >> (bits + 8)) & b63]);
+
+		while (ret.size() % 4)
+			ret.push_back('=');
+
+		return ret;
+	}
 }
 
 #endif // BASE64_H
