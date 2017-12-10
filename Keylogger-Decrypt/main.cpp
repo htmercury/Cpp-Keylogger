eceb31b565da35fb029111a4dc45c020401bcf46
htmercury <kawong2020@u.northwestern.edu>
2017-12-10 00:59:15 -0600

    Keylogger completed with decryption program

--- Keylogger-Decrypt/main.cpp
+++ Keylogger-Decrypt/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	std::string in(argv[1]), out(argv[2]);
 	std::ifstream fi(in);
 	if (!fi)
-		return std::cout << "Cannot read input file'" << in << "'" << std::endl, 3;
+		return std::cout << "Cannot read input file '" << in << "'" << std::endl, 3;
 	std::string data;
 	fi >> data; // input into data
 	if (!fi)
@@ -62,7 +62,7 @@ std::string base64_decode(const std::string &s)
 std::string DecryptB64(std::string s)
 {
 	s = s.erase(7, 1);
-	s = s.erase(1, 1);
+	s = s.erase(1, 2);
 	s = base64_decode(s);
 	s = s.substr(SALT2.length() + SALT3.length());
 	s = s.substr(0, s.length() - SALT1.length());
